c4f8fd3827b3a52066928993bd35a1f73fc072a4
bartosz121 <bmagiera121@gmail.com>
2021-09-04 14:59:03 +0000

    Add custom `ModelAdmin` for `Post` model

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from django.db.models import Count
+from .models import Post
 
 # Register your models here.
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "author",
+        "with_picture",
+        "short_body",
+        "likes",
+        "created",
+        "updated",
+    )
+
+    list_filter = ("author", "liked")
+    search_fields = ("body",)
+    date_hierarchy = "created"
+    ordering = (
+        "id",
+        "created",
+        "updated",
+    )
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        queryset = queryset.annotate(
+            _likes=Count("liked", distinct=True),
+        )
+        return queryset
+
+    def likes(self, obj):
+        return obj._likes
+
+    likes.admin_order_field = "_likes"
+
+    def short_body(self, obj):
+        return f"{obj.body[:15]}..."
+
+    def with_picture(self, obj):
+        return bool(obj.picture)
