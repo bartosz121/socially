6f1be590d9de01c22577382ad972bb68155a8dcb
bartosz121 <bmagiera121@gmail.com>
2021-09-04 14:53:41 +0000

    Add `Profile` model

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_picture = models.ImageField(
+        upload_to="profile_pictures", default="profile_pictures/default.png"
+    )
+    profile_background = models.ImageField(
+        upload_to="profile_backgrounds",
+        default="profile_backgrounds/default.png",
+    )
+    bio = models.TextField(blank=True)
+    following = models.ManyToManyField(
+        User, related_name="following", blank=True
+    )
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"#{self.user.pk} - {self.user.username}"
+
+    def get_posts(self):
+        return self.posts.all()
+
+    @property
+    def posts_count(self):
+        return self.posts.all().count()
