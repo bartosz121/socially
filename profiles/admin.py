1c4949300235da94cb7f43f3493a70739a542c65
bartosz121 <bmagiera121@gmail.com>
2021-09-04 15:29:58 +0000

    Add cutsom `ModelAdmin` for `Profile`

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,6 +1,42 @@
 from django.contrib import admin
+from django.db.models import Count
 from .models import Profile
 
 # Register your models here.
 
-admin.site.register(Profile)
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "followers",
+        "short_bio",
+        "profile_picture",
+        "profile_background",
+        "created",
+        "updated",
+    )
+
+    list_filter = ("user",)
+    search_fields = ("bio",)
+    date_hierarchy = "created"
+    ordering = (
+        "created",
+        "updated",
+    )
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        queryset = queryset.annotate(
+            _followers=Count("following", distinct=True),
+        )
+        return queryset
+
+    def followers(self, obj):
+        return obj._followers
+
+    followers.admin_order_field = "_followers"
+
+    def short_bio(self, obj):
+        return obj.bio[:15]
