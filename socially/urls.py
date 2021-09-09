9b7bf15fc0ed107681715766c0031f68b828f1fb
Bartosz <bmagiera121@gmail.com>
2021-09-09 14:37:18 +0200

    Add `accounts` app to urls

--- socially/urls.py
+++ socially/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("posts.urls", namespace="posts")),
+    path("accounts/", include("accounts.urls", namespace="accounts")),
     path("profiles/", include("profiles.urls", namespace="profiles")),
 ]
 
