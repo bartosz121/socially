c56d3d09c40478fbe6ce36ccfc7b10e26686fb92
Bartosz <bmagiera121@gmail.com>
2021-09-09 14:28:37 +0200

    Add `accounts` urls
    - `register/`
    - `settings/`
    
    Built-in class views:
    
    - `login/`
    - `logout/`

---
+++ accounts/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from .views import register, SettingsView
+
+app_name = "accounts"
+
+urlpatterns = [
+    path("register/", register, name="registration"),
+    path("settings/", SettingsView.as_view(), name="settings"),
+    path("login/", auth_views.LoginView.as_view(), name="login"),
+    path(
+        "logout/",
+        auth_views.LogoutView.as_view(
+            template_name="registration/logout.html"
+        ),
+        name="logout",
+    ),
+]
