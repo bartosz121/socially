7ac7df9761aed879c7156d99d2cbcb5b373703be
bartosz121 <bmagiera121@gmail.com>
2021-09-11 20:51:19 +0200

    Remove `console.log`
    - Change variable name to make more sense

--- static/accounts/showAlerts.js
+++ static/accounts/showAlerts.js
@@ -1,19 +1,17 @@
 window.onload = () => {
   const alerts = document.querySelectorAll('.alert,.invalid-feedback')
   if (alerts.length === 0) {
-    console.log('no alerts')
     return;
   }
-  console.log('alerts present')
   Array.from(alerts).forEach(alert => {
-    let collapseElement = alert.parentElement;
-    while (!collapseElement.classList.contains('collapse')) {
-      collapseElement = collapseElement.parentElement
+    let element = alert.parentElement;
+    while (!element.classList.contains('collapse')) {
+      element = element.parentElement
       // failsafe if something goes wrong
-      if (collapseElement.localName === 'body') {
+      if (element.localName === 'body') {
         return;
       }
     }
-    collapseElement.classList.add('show')
+    element.classList.add('show')
   });
 }
