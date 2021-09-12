1885a8fb36db2ae1d43f5e9a284444f5e526c8bf
bartosz121 <bmagiera121@gmail.com>
2021-09-12 15:56:59 +0200

    Add `profiles/views.py'
    - Add `ProfileDetailView` view

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from profiles.models import Profile
 
 # Create your views here.
+
+
+class ProfileDetailView(DetailView):
+    model = Profile
+    template_name = "profiles/detail.html"
+    slug_field = "username"
+    slug_url_kwarg = "username"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["posts"] = context["profile"].get_posts()
+        return context
