6f09f1561d0db0391ed7e3f91f55179fa8fc616a
bartosz121 <bmagiera121@gmail.com>
2021-09-13 17:05:06 +0200

    Add `main.js`
    - Add  `checkImages` function which shrinks post images if they are too big (> 600x400)
    - Add `createOnClickModalImages` function which iterates over images with `modal-img` class and adds `click` event listener that shows bootstrap modal with the image inside

---
+++ static/main.js
@@ -0,0 +1,27 @@
+const checkImages = () => {
+  const images = document.querySelectorAll('.post-img');
+  images.forEach(image => {
+    if (image.width > 600 || image.height > 400) {
+      image.style.width = '600px';
+      image.style.height = '400px';
+      image.style.objectFit = 'scale-down';
+    }
+  });
+}
+
+const createOnClickModalImages = () => {
+  const modal = new bootstrap.Modal(document.getElementById('imageModal'))
+  const modalImgTag = document.getElementById('modal-body-img');
+  const images = document.querySelectorAll('.modal-img');
+  images.forEach(image => {
+    image.addEventListener('click', () => {
+      modalImgTag.src = image.src;
+      modal.show()
+    })
+  })
+}
+
+window.onload = () => {
+  checkImages()
+  createOnClickModalImages()
+}
