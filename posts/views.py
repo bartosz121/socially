39053102d7a69df70b7d30e0f70d1956db67f2b7
bartosz121 <bmagiera121@gmail.com>
2021-09-15 21:58:23 +0200

    Add `HandleLike` view
    - Accepts `pk` parameter which is used to query `Post` object
    - Adds `request.user` to `Post.liked`
    - Returns `JsonResponse` with `value` (like/dislike) and `likes` (`post.like_count`)

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
-from django.contrib.auth.models import User
+from django.http import JsonResponse
+from django.shortcuts import redirect, render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import View
 from django.views.generic.detail import DetailView
-from profiles.models import Profile
 from .models import Post
 
 # Create your views here.
@@ -13,21 +14,43 @@ def home_view(request):
         return render(
             request,
             "posts/main.html",
-            {"followed_users_posts": followed_users_posts},
+            {"posts": followed_users_posts},
         )
     else:
         return render(
             request,
             "posts/main.html",
-            {"followed_users_posts": Post.objects.all().order_by("-created")},
+            {"posts": Post.objects.all().order_by("-created")},
         )
 
 
-class PostDetailView(DetailView):
-    model = Post
-    template_name = "posts/detail.html"
+# class PostDetailView(DetailView):
+#     model = Post
+#     template_name = "posts/detail.html"
 
-    def get_queryset(self, *args, **kwargs):
-        user = User.objects.get(username=self.kwargs["author_username"])
-        profile = Profile.objects.get(user=user)
-        return super().get_queryset(*args, **kwargs).filter(author=profile)
+#     def get_queryset(self, *args, **kwargs):
+#         user = User.objects.get(username=self.kwargs["author_username"])
+#         profile = Profile.objects.get(user=user)
+#         return super().get_queryset(*args, **kwargs).filter(author=profile)
+
+
+class HandleLike(LoginRequiredMixin, View):
+    def post(self, request, pk, *args, **kwargs):
+        # TODO user can delete post and it will still be visible meanwhile user likes -> this throws error
+        post = Post.objects.get(pk=pk)
+        response = {}
+
+        if not post.get_user_liked(request.user):
+            response["value"] = "like"
+            post.liked.add(request.user)
+        else:
+            response["value"] = "dislike"
+            post.liked.remove(request.user)
+
+        response["likes"] = post.like_count
+
+        return JsonResponse(response, safe=False)
+
+    def get(self, request, pk, *args, **kwargs):
+        # TODO redirect to post detail
+        return redirect("posts:home-view")
