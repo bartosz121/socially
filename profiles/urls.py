e7e57a9efafc0ba3818861f8f6c153266ac740bd
bartosz121 <bmagiera121@gmail.com>
2021-09-16 18:01:43 +0200

    Add `HandleFollow` url

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path, re_path
-from .views import ProfileDetailView
+from .views import ProfileDetailView, HandleFollow
 
 app_name = "profiles"
 
 urlpatterns = [
     path(
-        r"<slug:username>/",
+        "<slug:username>/",
         ProfileDetailView.as_view(),
         name="profile-detail",
+    ),
+    path(
+        "handlefollow/<int:pk>", HandleFollow.as_view(), name="handle-follow"
     ),
 ]
