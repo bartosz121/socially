84e3f2d034bc142f2c4c061b3cae69a9217c2825
bartosz121 <bmagiera121@gmail.com>
2021-09-16 17:57:35 +0200

    Add `HandleFollow` view
    - expects POST request with `target user` pk then adds/removes him from calling user following list

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import redirect
+from django.views import View
 from django.views.generic.detail import DetailView
-from profiles.models import Profile
+from django.contrib.auth.mixins import LoginRequiredMixin
+from accounts.models import CustomUser
+from .models import Profile
 
 # Create your views here.
 
@@ -15,3 +19,26 @@ class ProfileDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         context["posts"] = context["profile"].get_posts()
         return context
+
+
+class HandleFollow(LoginRequiredMixin, View):
+    def post(self, request, pk, *args, **kwargs):
+        calling_user = CustomUser.objects.get(pk=request.user.pk)
+        target_user = CustomUser.objects.get(pk=pk)
+        response = {}
+
+        if not calling_user.profile.get_user_follow_status(target_user):
+            response["value"] = "follow"
+            calling_user.profile.following.add(target_user)
+        else:
+            response["value"] = "unfollow"
+            calling_user.profile.following.remove(target_user)
+
+        response["followers"] = target_user.profile.followers_count
+
+        return JsonResponse(response, safe=False)
+
+    def get(self, request, pk, *args, **kwargs):
+        return redirect(
+            "profiles:profile-detail", request.user.profile.username
+        )
