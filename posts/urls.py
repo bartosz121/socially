5052ae5b1b7359f5b2f11b278ed6d09209cd011f
bartosz121 <bmagiera121@gmail.com>
2021-09-17 22:46:28 +0200

    Add `HomeView` to urls

--- posts/urls.py
+++ posts/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import home_view, HandleLike
+from .views import HomeView, HandleLike
 
 app_name = "posts"
 
 urlpatterns = [
-    path("", home_view, name="home-view"),
+    path("", HomeView.as_view(), name="home-view"),
     path("handlelike/<int:pk>", HandleLike.as_view(), name="handle-like"),
 ]
