c2656386ad3ca24cfae2b63c751e70f8133aae2b
bartosz121 <bmagiera121@gmail.com>
2021-10-01 21:12:34 +0200

    Remove `checkImages` function from `main.js` added the same functionality in CSS

--- static/main.js
+++ static/main.js
@@ -1,14 +1,3 @@
-const checkImages = () => {
-  const images = document.querySelectorAll('.post-img');
-  images.forEach(image => {
-    if (image.width > 600 || image.height > 400) {
-      image.style.width = '600px';
-      image.style.height = '400px';
-      image.style.objectFit = 'scale-down';
-    }
-  });
-}
-
 const createOnClickModalImages = () => {
   const modal = new bootstrap.Modal(document.getElementById('imageModal'))
   const modalImgTag = document.getElementById('modal-body-img');
@@ -22,10 +11,5 @@ const createOnClickModalImages = () => {
 }
 
 window.onload = () => {
-  checkImages()
   createOnClickModalImages()
-}
-
-window.onresize = () => {
-  checkImages()
 }
