0c4cc1b72a4c8999c7a59a41f0a0bf01c451a782
bartosz121 <bmagiera121@gmail.com>
2021-10-30 21:29:24 +0200

    Rename `get_posts_paginator_hx` to `get_paginator_hx`
    - It now takes `template` argument which is used in returned TemplateResponse

--- posts/decorators.py
+++ posts/decorators.py
@@ -3,31 +3,30 @@ from django.http.response import HttpResponse
 from django.template.response import TemplateResponse
 
 
-def get_posts_paginator_hx(func):
-    """Get 'posts_hx.html' with paginator from queryset; used in htmx infinite scroll"""
+def get_paginator_hx(template):
+    def decorate(func):
+        def wrap(request, *args, **kwargs):
+            qs = func(request, *args, **kwargs)
+            page = request.GET.get("page", 1)
+            hx_get_url = request.build_absolute_uri(request.path)
+            paginator = Paginator(qs, 10)
 
-    def wrap(request, *args, **kwargs):
-        qs = func(request, *args, **kwargs)
-        page = request.GET.get("page", 1)
-        hx_get_url = request.build_absolute_uri(
-            request.path
-        )  # used in posts_hx.html to get next page
-        paginator = Paginator(qs, 10)
+            try:
+                result = paginator.page(page)
+            except PageNotAnInteger:
+                result = paginator.page(1)
+            except EmptyPage:
+                result = paginator.page(paginator.num_pages)
 
-        try:
-            posts = paginator.page(page)
-        except PageNotAnInteger:
-            posts = paginator.page(1)
-        except EmptyPage:
-            posts = paginator.page(paginator.num_pages)
+            if len(result) == 0:
+                return HttpResponse("<h3>Result not found</h3>")
 
-        if len(posts) == 0:
-            return HttpResponse("<h3>Posts not found</h3>")
+            return TemplateResponse(
+                request,
+                template,
+                {"result": result, "hx_get_url": hx_get_url},
+            )
 
-        return TemplateResponse(
-            request,
-            "htmx/_hx/posts_hx.html",
-            {"posts": posts, "hx_get_url": hx_get_url},
-        )
+        return wrap
 
-    return wrap
+    return decorate
