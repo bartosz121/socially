76d8bac68e0f9bf99be6a3833412a46c0190a88c
bartosz121 <bmagiera121@gmail.com>
2022-03-08 23:26:58 +0100

    Add Spinner component

---
+++ socially-web/src/components/spinner/Spinner.jsx
@@ -0,0 +1,9 @@
+function Spinner() {
+    return (
+        <div class="spinner-wrapper text-center">
+            <div class="spinner-border" role="status"></div>
+        </div>
+  )
+}
+
+export default Spinner
