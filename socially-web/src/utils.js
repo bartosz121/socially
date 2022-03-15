803b7ea13a8ffe15ec50a18ec0e6d96232be5602
bartosz121 <bmagiera121@gmail.com>
2022-03-15 21:56:47 +0100

    Add `copyTextToClipboard` function

--- socially-web/src/utils.js
+++ socially-web/src/utils.js
@@ -25,3 +25,11 @@ export const formatNumberToDisplay = (value) => {
 
   return numeral(value).format(format)
 }
+
+export async function copyTextToClipboard(text) {
+  if ('clipboard' in navigator) {
+    return await navigator.clipboard.writeText(text);
+  } else {
+    return document.execCommand('copy', true, text);
+  }
+}
