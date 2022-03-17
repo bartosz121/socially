a8623cb6dfaad149dbf79329417e8a3e04022e32
bartosz121 <bmagiera121@gmail.com>
2022-03-17 13:36:53 +0100

    Rename `following` to `followers`, remove unused methods

--- profiles/models.py
+++ profiles/models.py
@@ -23,7 +23,7 @@ class Profile(
     ResizeImageMixin,
 ):
     user = models.OneToOneField(
-        settings.AUTH_USER_MODEL,
+        User,
         on_delete=models.CASCADE,
         related_name="profile",
     )
@@ -49,8 +49,8 @@ class Profile(
     bio = models.TextField(
         max_length=180, help_text="Write something about yourself!", blank=True
     )
-    following = models.ManyToManyField(
-        settings.AUTH_USER_MODEL, related_name="following", blank=True
+    followers = models.ManyToManyField(
+        "Profile", related_name="following", blank=True
     )
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
@@ -58,17 +58,14 @@ class Profile(
     def __str__(self):
         return f"#{self.user.pk} - {self.username}"
 
-    @property
-    def posts_count(self):
+    def get_posts_count(self):
         return self.posts.all().count()
 
-    @property
-    def following_count(self):
-        return self.following.all().count()
+    def get_following_count(self):
+        return self.following.count()
 
-    @property
-    def followers_count(self):
-        return len(self.get_followers())
+    def get_followers_count(self):
+        return self.followers.count()
 
     def save(self, *args, **kwargs):
         self.resize(self.profile_picture.path, settings.PROFILE_PICTURE_SIZE)
@@ -81,53 +78,6 @@ class Profile(
         return reverse(
             "profiles:profile-detail", kwargs={"username": self.username}
         )
-
-    def get_posts(self):
-        return self.posts.all().order_by("-created")
-
-    def get_following_profiles(self):
-        return list(
-            Profile.objects.get(user=user) for user in self.following.all()
-        )
-
-    def get_user_follow_status(self, target_user):
-        """Is 'this' user following given user"""
-        return target_user.profile in self.get_following_profiles()
-
-    def get_following_users_posts(self):
-        # for user in self.get_following_profiles():
-        #     posts.append(Post.objects.filter(author=user))
-        # if len(posts) > 0:
-        #     return sorted(
-        #         chain(*posts), reverse=True, key=lambda post: post.created
-        #     )
-        posts = (
-            Post.objects.all()
-            .filter(author__in=self.get_following_profiles())
-            .order_by("-created")
-        )
-        return posts
-
-    def get_followers(self):
-        qs = Profile.objects.all().exclude(user=self.user)
-        followers = [
-            profile
-            for profile in qs
-            if self in profile.get_following_profiles()
-        ]
-        return followers
-
-    # TODO make this more 'smart'
-    # check for following in 'this' user following users
-    def get_follow_suggestions(self):
-        profiles = Profile.objects.all().exclude(user=self.user)
-        following = [*self.get_following_profiles()]
-        profiles = list(
-            filter(lambda profile: profile not in following, profiles)
-        )
-        if len(profiles) > 3:
-            return profiles[:3]
-        return profiles
 
 
 def user_did_save(sender, instance, created, *args, **kwargs):
