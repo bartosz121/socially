1ecac98706fbc6e4f3eaa910bc7cb723678e77a0
bartosz121 <bmagiera121@gmail.com>
2022-03-18 13:39:04 +0100

    Add `following` and `followers` actions to viewset

--- profiles/viewsets.py
+++ profiles/viewsets.py
@@ -2,11 +2,14 @@ from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
 from rest_framework import mixins
 from rest_framework import permissions
+from rest_framework.decorators import action
 from rest_framework.response import Response
 
+from accounts.models import CustomUser
 from api.permissions import IsAuthorOrIsStaffOrReadOnly
+from api.utils import get_paginated_queryset_response
 from .models import Profile
-from .serializers import ProfileSerializer
+from .serializers import ProfileSerializer, ProfileBasicSerializer
 
 
 class ProfileViewSet(
@@ -15,7 +18,7 @@ class ProfileViewSet(
     viewsets.GenericViewSet,
 ):
     """
-    ViewSet for retrieving and updating profile
+    Profile viewset
     """
 
     queryset = Profile.objects.all()
@@ -27,12 +30,44 @@ class ProfileViewSet(
 
     def list(self, request, *args, **kwargs):
         queryset = self.get_queryset()
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
+
+        return get_paginated_queryset_response(
+            self.paginator,
+            request,
+            queryset,
+            ProfileSerializer,
+        )
 
     def retrieve(self, request, pk=None):
-        queryset = self.get_queryset()
+        """Retrieve profile by user id"""
+        queryset = CustomUser.objects.all()
 
-        profile = get_object_or_404(queryset, pk=pk)
-        serializer = self.get_serializer(profile)
+        user = get_object_or_404(queryset, pk=pk)
+        serializer = self.get_serializer(user.profile)
         return Response(serializer.data)
+
+    @action(methods=["GET"], detail=True)
+    def following(self, request, pk=None, *args, **kwargs):
+        """Returns profiles of users that given profile is following"""
+        queryset = self.get_queryset()
+        profile = get_object_or_404(queryset, pk=pk)
+
+        return get_paginated_queryset_response(
+            self.paginator,
+            request,
+            profile.following.all(),
+            ProfileBasicSerializer,
+        )
+
+    @action(methods=["GET"], detail=True)
+    def followers(self, request, pk=None, *args, **kwargs):
+        """Returns user followers"""
+        queryset = self.get_queryset()
+        profile = get_object_or_404(queryset, pk=pk)
+
+        return get_paginated_queryset_response(
+            self.paginator,
+            request,
+            profile.followers.all(),
+            ProfileBasicSerializer,
+        )
