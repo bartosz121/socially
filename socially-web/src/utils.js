740971b4bf85702963f4ec1ca47cac562d28c5b6
bartosz121 <bmagiera121@gmail.com>
2022-03-19 21:28:53 +0100

    Add `getFormData` function

--- socially-web/src/utils.js
+++ socially-web/src/utils.js
@@ -26,10 +26,15 @@ export const formatNumberToDisplay = (value) => {
   return numeral(value).format(format)
 }
 
-export async function copyTextToClipboard(text) {
+export const copyTextToClipboard = async (text) => {
   if ('clipboard' in navigator) {
     return await navigator.clipboard.writeText(text);
   } else {
     return document.execCommand('copy', true, text);
   }
+}
+
+export const getFormData = (event) => {
+  const data = new FormData(event.currentTarget)
+  return data;
 }
