1e3efa45793e43c565807013c2c1bbb0218a2f20
bartosz121 <bmagiera121@gmail.com>
2022-03-20 23:03:26 +0100

    Add components to main.jsx

--- socially-web/src/main.jsx
+++ socially-web/src/main.jsx
@@ -1,12 +1,65 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import PostList from "./components/post-list/PostList";
+import HomeSection from "./components/HomeSection/HomeSection";
+import ProfileDetailPage from "./pages/ProfileDetailPage/ProfileDetailPage";
+import PostDetailPage from "./components/PostDetail/PostDetail";
+import PostList from "./components/PostList/PostList";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <PostList />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+import "./index.css";
+import FollowSuggestions from "./components/FollowSuggestions/FollowSuggestions";
+import PopularPosts from "./components/PopularPosts/PopularPosts";
+
+// Home section feed
+const homeSectionElement = document.getElementById("home-section");
+
+if (homeSectionElement) {
+  ReactDOM.render(
+    <HomeSection {...homeSectionElement.dataset} />,
+    homeSectionElement
+  );
+}
+
+// Explore page feed
+const exploreFeedElement = document.getElementById("explore-feed");
+
+if (exploreFeedElement) {
+  ReactDOM.render(
+    <PostList newestPost={null} {...exploreFeedElement.dataset} />,
+    exploreFeedElement
+  );
+}
+
+// Follow Suggestions
+const followSuggestionsElement = document.getElementById("follow-suggestions");
+
+if (followSuggestionsElement) {
+  ReactDOM.render(
+    <FollowSuggestions {...followSuggestionsElement.dataset} />,
+    followSuggestionsElement
+  );
+}
+
+// Popular Posts
+const popularPostsElement = document.getElementById("popular-posts");
+
+if (popularPostsElement) {
+  ReactDOM.render(<PopularPosts />, popularPostsElement);
+}
+
+// Post Detail
+const postDetailElement = document.getElementById("post-detail");
+if (postDetailElement) {
+  ReactDOM.render(
+    <PostDetailPage {...postDetailElement.dataset} />,
+    postDetailElement
+  );
+}
+
+// Profile Detail
+const profileDetailElement = document.getElementById("profile-detail");
+if (profileDetailElement) {
+  ReactDOM.render(
+    <ProfileDetailPage {...profileDetailElement.dataset} />,
+    profileDetailElement
+  );
+}
