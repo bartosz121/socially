4a6c64d3e058be9ef3f6a8fead2dafe19bb87538
bartosz121 <bmagiera121@gmail.com>
2022-06-29 17:04:44 +0200

    Remove socially-web

--- socially-web/src/components/spinner/Spinner.jsx
+++
@@ -1,9 +0,0 @@
-function Spinner() {
-  return (
-    <div className="spinner-wrapper text-center">
-      <div className="spinner-border" role="status"></div>
-    </div>
-  );
-}
-
-export default Spinner;
