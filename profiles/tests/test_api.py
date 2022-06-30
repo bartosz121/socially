f97a5d041cb44fe0ab24eb26c8bdeb6df1c7597f
Bartosz <28759224+bartosz121@users.noreply.github.com>
2022-06-30 13:27:53 +0200

    Add api tests (#8)
    * Fix accounts api tests
    
    - changed user.id to user.profile.username bcs of the api change
    
    * Fix status code from forbidden to unauthorized
    
    * Change user.id to user.profile.username
    
    * Fix test_follow_suggestions
    
    its checking the existence of ids in expected result instead of checking for equality in zip(expected, result)
    
    * Add viewset tests
    
    * Add viewset tests

--- profiles/tests/test_api.py
+++ profiles/tests/test_api.py
@@ -10,7 +10,7 @@ def test_follow(user, auth_client):
 
     # follow
     response = auth_client.post(
-        f"/api/v1/profiles/{test_user.profile.id}/follow/",
+        f"/api/v1/profiles/{test_user.profile.username}/follow/",
         {"action": "follow"},
     )
     data = response.data
@@ -20,7 +20,7 @@ def test_follow(user, auth_client):
 
     # unfollow
     response = auth_client.post(
-        f"/api/v1/profiles/{test_user.profile.id}/follow/",
+        f"/api/v1/profiles/{test_user.profile.username}/follow/",
         {"action": "unfollow"},
     )
 
@@ -36,7 +36,7 @@ def test_following(user, client):
 
     test_user.profile.followers.add(user.profile)
 
-    response = client.get(f"/api/v1/profiles/{user.id}/following/")
+    response = client.get(f"/api/v1/profiles/{user.profile.username}/following/")
     assert response.status_code == 200
 
     data = response.data["results"]
@@ -51,7 +51,7 @@ def test_followers(user, client):
 
     user.profile.followers.add(test_user.profile)
 
-    response = client.get(f"/api/v1/profiles/{user.id}/followers/")
+    response = client.get(f"/api/v1/profiles/{user.profile.username}/followers/")
     assert response.status_code == 200
 
     data = response.data["results"]
@@ -70,17 +70,17 @@ def test_follow_suggestions(user, client):
         CustomUserFactory(),
         CustomUserFactory(),
     ]
-    expected_suggestions.reverse()  # for zip in assert
 
-    response = client.get(f"/api/v1/profiles/{user.id}/follow-suggestions/")
+    expected_ids = [user.id for user in expected_suggestions]
+
+    response = client.get(f"/api/v1/profiles/{user.profile.username}/follow-suggestions/")
     assert response.status_code == 200
 
     data = response.data
 
     assert all(
         [
-            r_obj["user_id"] == u_obj.id
-            for r_obj, u_obj in zip(data, expected_suggestions)
+            response_suggestion["user_id"] in expected_ids for response_suggestion in data
         ]
     )
 
@@ -92,7 +92,7 @@ def test_is_user_following(user, client):
     user.profile.following.add(test_user.profile)
 
     response = client.get(
-        f"/api/v1/profiles/{test_user.id}/is-following/{user.id}/"
+        f"/api/v1/profiles/{test_user.profile.username}/is-following/{user.profile.username}/"
     )
     assert response.status_code == 200
     assert response.data["is_following"] == True
@@ -100,7 +100,7 @@ def test_is_user_following(user, client):
     # unfollow
     user.profile.following.remove(test_user.profile)
     response = client.get(
-        f"/api/v1/profiles/{test_user.id}/is-following/{user.id}/"
+        f"/api/v1/profiles/{test_user.profile.username}/is-following/{user.profile.username}/"
     )
     assert response.status_code == 200
     assert response.data["is_following"] == False
@@ -126,3 +126,50 @@ def test_most_followers(user, client):
 
     assert data[0]["user_id"] == most_followed_user.id
     assert data[1]["user_id"] == user.id
+
+@pytest.mark.django_db
+def test_viewset_list(client):
+    PAGINATED_RESULT_ITEM_COUNT = 10
+
+    profiles = [CustomUserFactory().profile for _ in range(15)]
+    profiles.reverse()
+    profiles_usernames = [profile.username for profile in profiles[:PAGINATED_RESULT_ITEM_COUNT]]
+
+    response = client.get("/api/v1/profiles/")
+    assert response.status_code == 200
+
+    data = response.data
+    assert data["next"].endswith("?page=2")
+    assert data["previous"] is None
+    assert data["count"] == len(profiles)
+
+    assert all([expected_username == p["username"] for expected_username, p in zip(profiles_usernames, data["results"])])
+
+
+
+@pytest.mark.django_db
+def test_viewset_retrieve(client):
+    profile = CustomUserFactory().profile
+    response = client.get(f"/api/v1/profiles/{profile.username}/")
+    assert response.status_code == 200
+    assert response.data["username"] == profile.username
+
+@pytest.mark.django_db
+def test_follow_stats(client):
+    follow_target = CustomUserFactory()
+
+    # 6 followers and 5 following
+    for i in range(11):
+        user = CustomUserFactory()
+        if i % 2 == 0:
+            follow_target.profile.followers.add(user.profile)
+        else:
+            user.profile.followers.add(follow_target.profile)
+
+    response = client.get(f"/api/v1/profiles/{follow_target.profile.username}/follow/count/")
+    assert response.status_code == 200
+
+    data = response.data
+    assert data["user_id"] == follow_target.id
+    assert data["followers"] == 6
+    assert data["following"] == 5
